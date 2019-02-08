53bfa795c8b350abbad10cbe65ca7f2a5a5e0b28
thquyen11 <thquyen11@hotmail.com>
2019-02-07 23:28:16 -0500

    Updates

---
+++ precache-manifest.34a7dcab67dee1a127be22735dff7be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c0d42cd7007ce4cc77c31f1833ef59",
+    "url": "/quyenaholic.com/static/media/profilePic.e3c0d42c.png"
+  },
+  {
+    "revision": "5930dc28087494df47bc26db6f2a6405",
+    "url": "/quyenaholic.com/static/media/background-img.5930dc28.jpg"
+  },
+  {
+    "revision": "4896828de86fac042677",
+    "url": "/quyenaholic.com/static/js/runtime~main.4896828d.js"
+  },
+  {
+    "revision": "f44b35100af0021d2c27",
+    "url": "/quyenaholic.com/static/js/main.f44b3510.chunk.js"
+  },
+  {
+    "revision": "3b2d78d5aaa0ddcc9d51",
+    "url": "/quyenaholic.com/static/js/1.3b2d78d5.chunk.js"
+  },
+  {
+    "revision": "f44b35100af0021d2c27",
+    "url": "/quyenaholic.com/static/css/main.6fba1fef.chunk.css"
+  },
+  {
+    "revision": "427cc9dfbffbda043710cee16a89a79f",
+    "url": "/quyenaholic.com/index.html"
+  }
+];
